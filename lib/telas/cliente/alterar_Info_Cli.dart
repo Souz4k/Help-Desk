fb7a66886c89a0d9c6b9311a43612d7271d678cd
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-10-25 12:40:10 -0300

    atualizando a tela de informações do cliente

--- lib/telas/cliente/alterar_Info_Cli.dart
+++ lib/telas/cliente/alterar_Info_Cli.dart
@@ -1,18 +1,28 @@
+import 'package:app/telas/cliente/tela_inicial_cliente.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class AlterarInfoCli extends StatefulWidget {
-  const AlterarInfoCli({Key? key});
+  const AlterarInfoCli({Key? key}) : super(key: key);
 
   @override
   _AlterarInfoCliState createState() => _AlterarInfoCliState();
 }
 
 class _AlterarInfoCliState extends State<AlterarInfoCli> {
+  final maskFormatter = MaskTextInputFormatter(
+    mask: '(##)#####-####',
+    filter: {
+      "#": RegExp(r'[0-9]'), // Permite apenas números
+    },
+  );
+
   late TextEditingController _nomeController;
   late TextEditingController _senhaAtualController;
   late TextEditingController _novaSenhaController;
@@ -48,6 +58,60 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
     }
   }
 
+  Future<void> _showErrorDialog(String errorMessage) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          title: Row(
+            children: [
+              Icon(Icons.error, color: Colors.red, size: 30),
+              SizedBox(width: 10),
+              Text(
+                'Erro',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  "Erro ao Atualizar Informações",
+                  style: TextStyle(fontSize: 18, color: Colors.black87),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                ),
+                child: Text(
+                  'OK',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   Future<void> _atualizarInformacoes(String newName, String oldPassword,
       String newSenha, String celular) async {
     try {
@@ -58,14 +122,17 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
         );
         await _user!.reauthenticateWithCredential(credential);
 
+        // Atualiza o nome, se necessário
         if (newName.isNotEmpty && newName != _nomeAtual) {
           await _user!.updateDisplayName(newName);
           _nomeAtual = newName;
         }
+        // Atualiza a senha, se fornecida
         if (newSenha.isNotEmpty) {
           await _user!.updatePassword(newSenha);
         }
 
+        // Atualiza o celular, se fornecido
         Map<String, String> updateData = {};
         if (celular.isNotEmpty) {
           updateData['celular'] = celular;
@@ -78,20 +145,11 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
               .set(updateData, SetOptions(merge: true));
         }
 
-        // Mostrar pop-up de sucesso
         _showSuccessDialog();
       }
     } catch (e) {
       print("Erro ao atualizar informações: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            "Erro ao atualizar informações",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.red,
-        ),
-      );
+      _showErrorDialog("Erro ao atualizar informações: ${e.toString()}");
     }
   }
 
@@ -102,15 +160,17 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
       if (returnedImage == null) return;
       File selectedImage = File(returnedImage.path);
 
+      // Faz o upload da imagem para o Firebase Storage
       final storageRef = FirebaseStorage.instance.ref();
       final imageRef = storageRef.child("${_user!.uid}.jpg");
       await imageRef.putFile(selectedImage);
       var url = await imageRef.getDownloadURL();
 
       setState(() {
-        _imageUrl = url;
+        _imageUrl = url; // Atualiza a URL da imagem no estado
       });
 
+      // Atualiza a URL da imagem no Firestore
       await FirebaseFirestore.instance
           .collection("users")
           .doc(_user!.uid)
@@ -126,11 +186,16 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
     }
   }
 
+  void _navigateToHomeScreen() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => telaInicialCliente()),
+    );
+  }
+
   Future<void> _showSuccessDialog() async {
     return showDialog<void>(
       context: context,
-      barrierDismissible:
-          false, // O usuário precisa pressionar OK para fechar o diálogo
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           shape: RoundedRectangleBorder(
@@ -142,10 +207,7 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
               SizedBox(width: 10),
               Text(
                 'Sucesso',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
             ],
           ),
@@ -154,10 +216,7 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
               children: <Widget>[
                 Text(
                   'Informações atualizadas com sucesso!',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black87,
-                  ),
+                  style: TextStyle(fontSize: 18, color: Colors.black87),
                 ),
               ],
             ),
@@ -167,6 +226,7 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
               child: ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).pop();
+                  _navigateToHomeScreen();
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
@@ -177,10 +237,7 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
                 ),
                 child: Text(
                   'OK',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
+                  style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
               ),
             ),
@@ -195,8 +252,8 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
-        title: Text("Alterar Informações"),
+        title: Text("Alterar Informações",
+            style: TextStyle(fontSize: 20, color: Colors.white)),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -206,59 +263,25 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
             children: [
               GestureDetector(
                 onTap: () => _showImageSourceActionSheet(context),
-                child: Container(
-                  width: 150,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    shape: BoxShape.circle,
-                  ),
-                  child: ClipOval(
-                    child: _imageUrl != null
-                        ? Image.network(
-                            _imageUrl!,
-                            fit: BoxFit.cover,
-                            width: 150,
-                            height: 150,
-                          )
-                        : Icon(Icons.camera_alt_outlined, size: 70),
-                  ),
+                child: CircleAvatar(
+                  radius: 75,
+                  backgroundColor: Colors.grey[300],
+                  backgroundImage:
+                      _imageUrl != null ? NetworkImage(_imageUrl!) : null,
+                  child: _imageUrl == null
+                      ? Icon(Icons.camera_alt_outlined,
+                          size: 70, color: Colors.grey[600])
+                      : null,
                 ),
               ),
               SizedBox(height: 20),
-              TextFormField(
-                controller: _nomeController,
-                decoration: InputDecoration(
-                  labelText: 'Nome',
-                  border: OutlineInputBorder(),
-                ),
-              ),
+              _buildTextField(_nomeController, 'Nome', false),
               SizedBox(height: 20),
-              TextFormField(
-                controller: _senhaAtualController,
-                decoration: InputDecoration(
-                  labelText: 'Senha Atual',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
+              _buildTextField(_senhaAtualController, 'Senha Atual', true),
               SizedBox(height: 20),
-              TextFormField(
-                controller: _novaSenhaController,
-                decoration: InputDecoration(
-                  labelText: 'Nova Senha',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
+              _buildTextField(_novaSenhaController, 'Nova Senha', true),
               SizedBox(height: 20),
-              TextFormField(
-                controller: _celularController,
-                decoration: InputDecoration(
-                  labelText: 'Celular',
-                  border: OutlineInputBorder(),
-                ),
-              ),
+              _buildCellphoneField(),
               SizedBox(height: 30),
               ElevatedButton(
                 onPressed: () async {
@@ -272,12 +295,13 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blueAccent,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
+                      borderRadius: BorderRadius.circular(30.0)),
                 ),
-                child: Text(
-                  "Atualizar Informações",
-                  style: TextStyle(color: Colors.white),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  child: Text("Atualizar Informações",
+                      style: TextStyle(color: Colors.white, fontSize: 16)),
                 ),
               ),
             ],
@@ -287,27 +311,65 @@ class _AlterarInfoCliState extends State<AlterarInfoCli> {
     );
   }
 
+  Widget _buildTextField(
+      TextEditingController controller, String labelText, bool obscureText) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(color: Colors.grey[700]),
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blueAccent)),
+        filled: true,
+        fillColor: Colors.grey[100],
+      ),
+      obscureText: obscureText,
+    );
+  }
+
+  Widget _buildCellphoneField() {
+    return TextFormField(
+      controller: _celularController,
+      decoration: InputDecoration(
+        labelText: 'Celular',
+        labelStyle: TextStyle(color: Colors.grey[700]),
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blueAccent)),
+        filled: true,
+        fillColor: Colors.grey[100],
+      ),
+      keyboardType: TextInputType.phone,
+      inputFormatters: [
+        FilteringTextInputFormatter.digitsOnly, // Permite apenas dígitos
+        maskFormatter, // Aplica a máscara
+      ],
+    );
+  }
+
   Future<void> _showImageSourceActionSheet(BuildContext context) async {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return SafeArea(
-          child: Wrap(
+        return Container(
+          height: 200,
+          child: Column(
             children: <Widget>[
               ListTile(
-                leading: Icon(Icons.photo_library),
-                title: Text('Galeria'),
+                leading: Icon(Icons.camera),
+                title: Text('Câmera'),
                 onTap: () {
-                  _pickImage(ImageSource.gallery);
-                  Navigator.of(context).pop();
+                  Navigator.pop(context);
+                  _pickImage(ImageSource.camera);
                 },
               ),
               ListTile(
-                leading: Icon(Icons.photo_camera),
-                title: Text('Câmera'),
+                leading: Icon(Icons.image),
+                title: Text('Galeria'),
                 onTap: () {
-                  _pickImage(ImageSource.camera);
-                  Navigator.of(context).pop();
+                  Navigator.pop(context);
+                  _pickImage(ImageSource.gallery);
                 },
               ),
             ],
